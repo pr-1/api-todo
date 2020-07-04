b863b69988739977fef6f7906ca88f98cb930efa
Prince <prince17.ps6@gmail.com>
2020-07-04 22:04:39 +0530

    al origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: "*",
   });
   await app.listen(process.env.PORT || 3000);
 }
