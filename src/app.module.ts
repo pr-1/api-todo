fd85b022d9e64cc50d1da91a43033e54460cc68d
Prince <prince17.ps6@gmail.com>
2020-07-04 22:15:27 +0530

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { AuthModule } from './modules/auth/auth.module';
-const dbUrl = 'mongodb+srv://blog-app-admin:qwerty1234@cluster0-cb4pe.mongodb.net/test?retryWrites=true&w=majority';
+const dbUrl = 'mongodb+srv://blog-app-admin:qwerty1234@cluster0.cb4pe.mongodb.net/test?retryWrites=true&w=majority';
 @Module({
   imports: [
     TypegooseModule.forRoot(dbUrl, {
